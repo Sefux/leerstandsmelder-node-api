227275becce6cdd5f6093caf1d5f12ad50efb1ee
Anton Koch <anton.koch@gmail.com>
2016-05-03 20:28:11 +0200

    status output of add scope script updated

--- bin/add-user-scope.js
+++ bin/add-user-scope.js
@@ -30,8 +30,11 @@ Promise.coroutine(function* () {
     if (user) {
         if (user.scopes.indexOf(argv.scope) === -1) {
             user.scopes.push(argv.scope);
+        } else {
+            console.log('No update of User necessary, has scope ' + argv.scope);
         }
         yield user.save();
+        console.log('Updated User scope.');
         let api_key = yield mongoose.model('ApiKey')
             .findOne({user_uuid: user.uuid, active: true})
             .sort('-created').exec();
@@ -39,10 +42,10 @@ Promise.coroutine(function* () {
             if (api_key.scopes.indexOf(argv.scope) === -1) {
                 api_key.scopes.push(argv.scope);
                 yield api_key.save();
-                console.log('Scope ' + argv.scope + ' added to user.');
+                console.log('Scope ' + argv.scope + ' added to ApiKey.');
                 process.exit(0);
             } else {
-                console.log('No update necessary, user has scope ' + argv.scope);
+                console.log('No update of ApiKey necessary, has scope ' + argv.scope);
                 process.exit(0);
             }
             process.exit(0);
